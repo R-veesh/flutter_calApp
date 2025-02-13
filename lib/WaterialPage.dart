aa5a50f0e0763fbe67c65830062715f39bb6a642
Rashking <raweeshalakruvan5@gmail.com>
2025-02-14 00:15:10 +0530

    button, padding & margin

--- lib/WaterialPage.dart
+++ lib/WaterialPage.dart
@@ -45,67 +45,92 @@ class CMaterialPage extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text(
-              'LKR convert Dolor',
-              style: TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
-                  color: Color.fromARGB(255, 0, 0, 200)),
+            Container(
+              padding: const EdgeInsets.all(10),
+              margin: const EdgeInsets.all(10),
+              // color: Colors.black,
+              child: Text(
+                'LKR convert Dolor',
+                style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    color: Color.fromARGB(255, 0, 0, 200)),
+              ),
             ),
-            TextField(
-              style: TextStyle(
-                color: Color.fromARGB(255, 38, 102, 175),
-                fontSize: 26,
-              ),
-              // decoration: InputDecoration(
-              //   label: Text(
-              //     'enter the LKR amount',
-              //     style: TextStyle(
-              //       color: Colors.amber,
-              //     ),
-              //   ),
-              // ),
+            Container(
+              // color: Colors.black,
+              margin: const EdgeInsets.all(10),
+              child: Padding(
+                padding: const EdgeInsets.fromLTRB(20, 10, 20, 10),
+                child: TextField(
+                  style: TextStyle(
+                    color: Color.fromARGB(255, 38, 102, 175),
+                    fontSize: 26,
+                  ),
+                  // decoration: InputDecoration(
+                  //   label: Text(
+                  //     'enter the LKR amount',
+                  //     style: TextStyle(
+                  //       color: Colors.amber,
+                  //     ),
+                  //   ),
+                  // ),
 
-              // decoration: InputDecoration(
-              //   labelText: 'good ',
-              // ),
+                  // decoration: InputDecoration(
+                  //   labelText: 'good ',
+                  // ),
 
-              // decoration: InputDecoration(
-              //   helperText: 'good ',
-              // ),
+                  // decoration: InputDecoration(
+                  //   helperText: 'good ',
+                  // ),
 
-              decoration: InputDecoration(
-                labelText: ' Enter The LKR Amount ',
-                hintText: '   LKR ',
-                hintStyle: TextStyle(
-                  color: Color.fromARGB(96, 31, 21, 182),
+                  decoration: InputDecoration(
+                    labelText: ' Enter The LKR Amount ',
+                    hintText: '   LKR ',
+                    hintStyle: TextStyle(
+                      color: Color.fromARGB(96, 31, 21, 182),
+                    ),
+                    prefix: Icon(Icons.monetization_on),
+                    filled: true,
+                    prefixIconColor: Color.fromARGB(255, 9, 106, 109),
+                    // focusedBorder: OutlineInputBorder(
+                    //   borderSide: BorderSide(
+                    //     color: Colors.black,
+                    //     width: 2.2,
+                    //     strokeAlign: BorderSide.strokeAlignCenter,
+                    //   ),
+                    //   borderRadius: BorderRadius.all(
+                    //     Radius.circular(16),
+                    //   ),
+                    // ),
+                    // enabledBorder: OutlineInputBorder(
+                    //   borderSide: BorderSide(
+                    //     color: Colors.black,
+                    //     width: 2.2,
+                    //     strokeAlign: BorderSide.strokeAlignCenter,
+                    //   ),
+                    //   borderRadius: BorderRadius.all(
+                    //     Radius.circular(16),
+                    //   ),
+                    // ),
+                    focusedBorder: border,
+                    enabledBorder: border,
+                    // contentPadding: cri,
+                  ),
+                  // keyboardType: TextInputType.number,
+                  keyboardType: TextInputType.numberWithOptions(
+                    decimal: true,
+                    // signed: true,
+                  ),
                 ),
-                prefix: Icon(Icons.monetization_on),
-                filled: true,
-                prefixIconColor: Color.fromARGB(255, 9, 106, 109),
-                // focusedBorder: OutlineInputBorder(
-                //   borderSide: BorderSide(
-                //     color: Colors.black,
-                //     width: 2.2,
-                //     strokeAlign: BorderSide.strokeAlignCenter,
-                //   ),
-                //   borderRadius: BorderRadius.all(
-                //     Radius.circular(16),
-                //   ),
-                // ),
-                // enabledBorder: OutlineInputBorder(
-                //   borderSide: BorderSide(
-                //     color: Colors.black,
-                //     width: 2.2,
-                //     strokeAlign: BorderSide.strokeAlignCenter,
-                //   ),
-                //   borderRadius: BorderRadius.all(
-                //     Radius.circular(16),
-                //   ),
-                // ),
-                focusedBorder: border,
-                enabledBorder: border,
               ),
+            ),
+            //button
+            TextButton(
+              onPressed: () {
+                print('done');
+              },
+              child: Text('Click me'),
             ),
           ],
         ),
